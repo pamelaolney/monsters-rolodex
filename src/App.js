1213e6c35fd97a91653adb49a01d3a486bfd4f4e
Pamela <pamela@joinhandshake.com>
2024-11-25 17:14:14 -0500

    Add monster objects

--- src/App.js
+++ src/App.js
@@ -9,8 +9,15 @@ class App extends Component {
 
       // start here
     this.state = {
-      name: {firstName: 'Pam', lastName: 'Olney'},
-      company: 'Google'
+      monster1: {
+        name: 'Ron',
+      },
+      monster2: {
+        name: 'Jim',
+      },
+      monster3: {
+        name: 'Maddie'
+      }
     }
   }
 
@@ -18,18 +25,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-             {/* change name */}
-            Hi {this.state.name.firstName} {this.state.name.lastName}, I work at {this.state.company}
-          </p>
-          <button onClick={() => {
-            this.setState({name: {firstName: 'Pamela', lastName: 'Christian'}})
-            console.log(this.state);
-            }
-          }>Change Name</button>
-        </header>
+       <h1>{this.state.monster1.name}</h1>
+       <h1>{this.state.monster2.name}</h1>
+       <h1>{this.state.monster.name}</h1>
       </div>
     );
   }
